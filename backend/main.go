d6c01bd957e7c8766d72e71166358c064a821c6c
Alex Saavedra <alexsaavedra2023@u.northwestern.edu>
2024-07-07 12:20:30 -0500

    update ports to 443 and TLS

--- backend/main.go
+++ backend/main.go
@@ -81,7 +81,7 @@ func setupRoutes() {
 		http.ServeFile(w, r, "build/index.html")
 	})
 
-	err := http.ListenAndServe(":80", corsHandler(http.DefaultServeMux))
+	err := http.ListenAndServeTLS(":443", "server.crt", "server.key", corsHandler(http.DefaultServeMux))
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
@@ -252,7 +252,7 @@ func main() {
 	dbutils.Dbsetup()
 	dbutils.Create_db_if_not_exists()
 	setupRoutes()
-	err := http.ListenAndServe(":80", nil)
+	err := http.ListenAndServeTLS(":443", "server.crt", "server.key", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
